626b83992c36b3c5d0b2b35d6e507b51586dd36c
Axel <axelsorensenwork@gmail.com>
2023-10-03 13:15:41 +0200

    vite pwa autoupdate

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/EeveeTest/',
-  plugins: [vue(),VitePWA({registerType: 'prompt', manifest: {
+  plugins: [vue(),VitePWA({registerType: 'autoUpdate', manifest: {
     "icons": [
       {
         "src": "icons/512.png",
